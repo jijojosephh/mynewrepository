d022a95dec1c5e32cb79dccce6f5c31a53900870
jijo <jijojoseph.k@gmail.com>
2022-02-17 16:56:07 +0530

    First commit

---
+++ lambda-java-example/src/main/java/Test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author jijo
+ *
+ */
+
+public class Test {
+	
+	/**
+	 * @param args
+	 */	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Executing Test java file");
+	}
+
+}
